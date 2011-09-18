6deef106085a4bf58789aa181966a3c74c924ef7
Vladimir Dronnikov <dronnikov@gmail.com>
2011-09-18 23:00:18 +0400

    initial commit

---
+++ example/sockjs/sockjs.js
@@ -0,0 +1,58 @@
+#!/usr/bin/env node
+'use strict';
+
+/**
+ * HTTP middleware
+ */
+
+var Stack = require('./lib');
+function stack() {
+return [
+  // report health status to load balancer
+  Stack.health(),
+  // serve static content
+  Stack.static(__dirname + '/public', 'index.html', {
+    maxAge: 0,
+  }),
+];
+}
+
+var WebSocketServer = require('sockjs').Server;
+var Connection = require('connection/sockjs');
+
+function Node(port) {
+  // web server
+  this.http = Stack.listen(stack(), {}, port);
+  // WebSocket server on top of web server
+  this.ws = new WebSocketServer({
+    sockjs_url: 'sockjs-latest.min.js',
+    // test
+    //disabled_transports: ['websocket']
+  });
+  // WebSocket connection handler
+  this.ws.installHandlers(this.http, {
+    prefix:'[/]ws'
+  });
+  this.ws.on('open', function(conn) {
+    // examine c in REPL
+    repl.c = conn;
+    // install default handlers
+    Connection.call(conn);
+    conn.on('you typed', function(val, aid) {
+      conn.ack(aid, val);
+    });
+  });
+  // notify
+  console.log('Listening to http://*:' + port + '. Use Ctrl+C to stop.');
+}
+
+// spawn workers
+var s1 = new Node(3001);
+var s2 = new Node(3002);
+var s3 = new Node(3003);
+var s4 = new Node(3004);
+
+// REPL for introspection
+var repl = require('repl').start('node> ').context;
+process.stdin.on('close', process.exit);
+repl.s1 = s1;
